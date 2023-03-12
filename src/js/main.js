f34d8636d5f2221edf332edc35ba37f4d864626f
mari <mariroman604@gmail.com>
2023-03-12 19:20:52 +0100

    addMask

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,18 @@
 import slider from "./modules/slider";
 import modals from "./modules/modals";
+import forms from "./modules/forms";
+import mask from "./modules/mask";
+import checkTextInputs from './modules/checkTextInputs';
+
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';  
     slider('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
     slider('.main-slider-item', '', 'vertical');
     modals();
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    
 });
