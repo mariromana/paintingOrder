aeec1572ff3174c1b71223fecdaf7752de6d9ff4
mari <mariroman604@gmail.com>
2023-03-12 20:33:49 +0100

    addShowMoreStyles

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ import modals from "./modules/modals";
 import forms from "./modules/forms";
 import mask from "./modules/mask";
 import checkTextInputs from './modules/checkTextInputs';
-
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';  
@@ -14,5 +14,6 @@ window.addEventListener('DOMContentLoaded', () => {
     mask('[name="phone"]');
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
     
 });
